c3569bf341a7440ebdc83ce600beefa2ab967397
mnishimori <mnishimori@yahoo.com.br>
2024-09-18 19:38:05 -0300

    refactor(account): refatoração da classe SignupService.java. (#2)

--- src/main/java/br/com/tecnoride/signup/SignupController.java
+++ src/main/java/br/com/tecnoride/signup/SignupController.java
@@ -2,31 +2,23 @@ package br.com.tecnoride.signup;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/signup")
 public class SignupController {
 
-    @Autowired
-    private SignupService signupService;
+  @Autowired
+  private SignupService signupService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public String signup(@RequestBody UserInput input) {
-        try {
-            int result = signupService.signup(input);
-            switch (result) {
-                case 1: return "Cadastro realizado com sucesso!";
-                case -1: return "CPF inválido.";
-                case -2: return "Email inválido.";
-                case -3: return "Nome inválido.";
-                case -4: return "Usuário já existe.";
-                case -5: return "Placa de carro inválida.";
-                default: return "Erro desconhecido.";
-            }
-        } catch (Exception e) {
-            return "Erro ao processar: " + e.getMessage();
-        }
-    }
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public String signup(@RequestBody UserInput input) {
+    signupService.signup(input);
+    return "Cadastro realizado com sucesso!";
+  }
 }
