e0fc5494a719877975b8393cbd222b890175699f
mnishimori <mnishimori@yahoo.com.br>
2024-09-29 18:33:44 -0300

    feat(getaccount): account service e get account use case. Testes

--- src/test/java/br/com/tecnoride/account/GetAccountByIdUseCaseTest.java
+++ src/test/java/br/com/tecnoride/account/GetAccountByIdUseCaseTest.java
@@ -1,6 +1,12 @@
 package br.com.tecnoride.account;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+import br.com.tecnoride.shared.validator.UuidValidator;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,8 +19,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class GetAccountByIdUseCaseTest {
 
+  public static final String FULANO_CICLANO_NAME = "Fulano Ciclano";
+  public static final String FULANO_CICLANO_EMAIL = "fulano.ciclano@domain.com";
+  public static final String FULANO_CICLANO_CPF = "62835691022";
+  public static final String CAR_PLATE = "ABC-1234";
+  public static final boolean IS_PASSENGER = true;
+  public static final boolean IS_DRIVER = false;
   @Mock
   private AccountService accountService;
+  @Mock
+  private UuidValidator uuidValidator;
   @InjectMocks
   private GetAccountByIdUseCase getAccountByIdUseCase;
 
@@ -22,16 +36,32 @@ class GetAccountByIdUseCaseTest {
   @NullAndEmptySource
   @ValueSource(strings = {"a", "1", "a1"})
   void shouldThrowExceptionWhenAccountIdIsInvalid(String id) {
-    Assertions.fail("shouldThrowExceptionWhenAccountIdIsInvalid");
+    doThrow(RuntimeException.class).when(uuidValidator).validate(id);
+
+    assertThatThrownBy(() -> getAccountByIdUseCase.execute(id))
+        .isInstanceOf(RuntimeException.class);
   }
 
   @Test
   void shouldThrowExceptionWhenAccountWasNotFoundById() {
-    Assertions.fail("shouldThrowExceptionWhenAccountWasNotFoundById");
+    var id = UUID.randomUUID().toString();
+    when(getAccountByIdUseCase.execute(id)).thenReturn(null);
+
+    var account = getAccountByIdUseCase.execute(id);
+
+    assertThat(account).isNull();
   }
 
   @Test
   void shouldGetAccountWhenAccountWasFoundById() {
-    Assertions.fail("shouldGetAccountWhenAccountWasFoundById");
+    var id = UUID.randomUUID();
+    var account = new Account(id, FULANO_CICLANO_EMAIL, FULANO_CICLANO_NAME, FULANO_CICLANO_CPF, CAR_PLATE,
+        IS_PASSENGER, IS_DRIVER);
+    when(accountService.findAccountById(id)).thenReturn(account);
+
+    var accountFound = getAccountByIdUseCase.execute(id.toString());
+
+    assertThat(accountFound).isNotNull();
+    assertThat(accountFound).usingRecursiveComparison().isEqualTo(accountFound);
   }
 }
